9652c4080e1cc8f76a617bb53973670082b44eee
Emiel van de Laar <gemiel@gmail.com>
2015-01-19 07:47:49 +0100

    Latest pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'E'
-SITENAME = 'idunno'
+AUTHOR = 'Emiel van de Laar'
+SITENAME = 'Galloping Alligator'
 SITEURL = ''
+
+PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -14,18 +16,22 @@ DEFAULT_LANG = 'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "../pelican-themes/basic"
